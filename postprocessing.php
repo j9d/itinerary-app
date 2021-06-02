4c87e4016d54e4450c3205cd7c94ffd7acf5047e
Jamie <jamiedimos@gmail.com>
2021-06-02 16:57:02 +1000

    Added form info to postprocessing page

--- postprocessing.php
+++ postprocessing.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'tools.php';
 
+// $user = query_login_table($_SESSION['current_email'])
+echo '<pre>' . print_r($_POST) . '</pre>';
 // Postprocessing for saving an itinerary to the database
 ?>
