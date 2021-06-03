26ce1ae8a98a99d56f3c1104f5a7b43dd6ebd4dd
Jamie <jamiedimos@gmail.com>
2021-06-03 17:21:32 +1000

    Added list of itineraries to user page, and summary page for single itinerary

--- user.php
+++ user.php
@@ -1,5 +1,17 @@
 <?php
 require_once 'tools.php';
+
+if (!isset($_SESSION['current_email'])) {
+    redirect('login.php');
+}
+
+$user = query_login_table($_SESSION['current_email']);
+if ($user['Item']['itineraries'] != null) {
+    $itineraries = $user['Item']['itineraries']['L'];
+} else {
+    $itineraries = null;
+}
+
 ?>
 
 <html>
@@ -8,6 +20,35 @@ require_once 'tools.php';
     </head>
     
     <body>
-        <p>User page</p>
+        <h1>Your Saved Itineraries</h1>
+        <hr/>
+
+        <?php
+        if ($itineraries) {
+            echo '<table>
+            <tr>
+                <th>Date</th>
+                <th>Origin</th>
+                <th>Number of Destinations</th>
+                <th>View</th>
+            </tr>
+            ';
+            foreach ($itineraries as $index => $itin) {
+                echo '<tr>
+                    <td>' . $itin['L'][0]['M']['date']['S'] . '</td>
+                    <td>' . $itin['L'][0]['M']['city']['S'] . '</td>
+                    <td>' . count($itin['L']) . '</td>
+                    <td><form method="post" action="itinerary.php">
+                        <input type="hidden" name="index" value="' . $index . '">
+                        <input type="submit" name="submit" value="View">
+                    </form></td>
+                </tr>';
+            }
+            echo '</table>';
+            
+        } else {
+            echo '<p>No itineraries to show.</p>';
+        }
+        ?>
     </body>
 </html>
