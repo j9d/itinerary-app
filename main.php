cbbb3969c6dafbfeac0790770827220adcecc887
s3719633 <manabat.louis@gmail.com>
2021-06-03 21:14:11 +1000

    [Task] Merged main.html into main.php
    Deleted html file after

--- main.php
+++ main.php
@@ -6,17 +6,77 @@ if (!isset($_SESSION['current_email'])) {
 }
 ?>
 
-<html>
-    <head>
-        <title>Itinerary App</title>
-    </head>
-    
-    <body>
-        <h1>Main Page</h1>
-        <p>Welcome, <?= $_SESSION['current_user'] ?>! (<a href='logout.php'>logout</a>)</p>
+<!DOCTYPE html>
+<html lang="en">
 
-        <br>
-        <p>View your <a href='user.php'>saved itineraries</a>.</p>
-        <p>Create a <a href='new_itinerary.php'>new itinerary</a>.</p>
-    </body>
+<head>
+    <title>Itinerary App</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid" id="main-title">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6 text-center">
+            <h1 id="title">Itinerary App</h1>
+        </div>
+        <div class="col-lg-3"></div>
+    </div>
+
+    <div class="row section container-fluid" id="navigation-bar">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-8">
+            <nav class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <ul class="nav navbar-nav mr-auto justify-content-end">
+                        <li class="nav-item">
+                            <a href='new_itinerary.php' id="nav-links">New itinerary</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href='user.php' id="nav-links">Past itineraries</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="col-lg-3">
+            <nav class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <ul class="nav navbar-nav mr-auto justify-content-end">
+                        <li class="nav-item">
+                            <p>Welcome, <?= $_SESSION['current_user'] ?>! (<a href='logout.php'>logout</a>)</p>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="row section container-fluid" id="main-body">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
+            <h2 id="page-title">Main Page</h2>
+            <br>
+
+            <p>View your <a href="user.php">saved itineraries</a>.</p>
+            <p>Create a <a href="new_itinerary.php">new itinerary</a>.</p>
+        </div>
+        <div class="col-lg-3"></div>
+    </div>
+</body>
+
+<footer class="container-fluid" id="footer">
+    <div>
+        <p>COSC2626 Cloud Computing - Assessment 3</p>
+        <p><span>&#169;</span>James Dimos (s3722398)<br>
+            <span>&#169;</span>Louis Manabat (s3719633)
+        </p>
+    </div>
+</footer>
+
 </html>
