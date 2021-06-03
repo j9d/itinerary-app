2921ca1572521672b4eb672504223507502332b6
s3719633 <manabat.louis@gmail.com>
2021-06-03 21:13:50 +1000

    [Task] Merged itinerary.html into itinerary.php
    Deleted html file after

--- itinerary.php
+++ itinerary.php
@@ -10,44 +10,105 @@ $user = query_login_table($_SESSION['current_email']);
 $itinerary = $user['Item']['itineraries']['L'][$index]['L'];
 ?>
 
-<html>
-    <head>
-        <title>Saved Itinerary</title>
-    </head>
-    
-    <body>
-        <h1>Saved Itinerary</h1>
-        <hr/>
-        <div>
-            <p><strong>Origin</strong></p>
-            <p><?= $itinerary[0]['M']['city']['S'] . ', ' . $itinerary[0]['M']['country']['S'] ?></p>
-            <p><strong>Depart on </strong><?= (new DateTime($itinerary[0]['M']['date']['S']))->format('jS F Y') ?></p>
-            <hr/>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Saved itineraries</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid" id="main-title">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6 text-center">
+            <h1 id="title">Saved Itineraries</h1>
         </div>
+        <div class="col-lg-3"></div>
+    </div>
 
-        <?php
-        foreach ($itinerary as $index => $location) {
-            if ($index > 0) {
-                $combinedname = $itinerary[$index]['M']['city']['S'] . ', ' . $itinerary[$index]['M']['country']['S'];
-                $arrdate = (new DateTime($itinerary[$index - 1]['M']['date']['S']))->format('jS F Y');
-                $depdate = (new DateTime($itinerary[$index]['M']['date']['S']))->format('jS F Y');
-                echo '<div>
-                    <p><strong>Destination ' . $index . '</strong></p>
-                    <p>' . $combinedname . '</p>
-                    <p><strong>Arrive on </strong>' . $arrdate . '</p>
-                    <p><strong>Depart on </strong>' . $depdate . '</p>
+    <div class="row section container-fluid" id="navigation-bar">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-8">
+            <nav class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <ul class="nav navbar-nav mr-auto justify-content-end">
+                        <li class="nav-item">
+                            <a href='new_itinerary.php' id="nav-links">New itinerary</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href='user.php' id="nav-links">Past itineraries</a>
+                        </li>
+                    </ul>
                 </div>
-                <hr/>';
+            </nav>
+        </div>
+        <div class="col-lg-3">
+            <nav class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <ul class="nav navbar-nav mr-auto justify-content-end">
+                        <li class="nav-item">
+                            <p>Welcome, <?= $_SESSION['current_user'] ?>! (<a href='logout.php'>logout</a>)</p>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="row section container-fluid" id="main-body">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
+            <h2 id="page-title">Saved Itinerary</h2>
+            <hr>
+            <div class="container-fluid">
+                <p><strong>Origin</strong></p>
+                <p><?= $itinerary[0]['M']['city']['S'] . ', ' . $itinerary[0]['M']['country']['S'] ?></p>
+                <p><strong>Depart on </strong><?= (new DateTime($itinerary[0]['M']['date']['S']))->format('jS F Y') ?></p>
+                <hr>
+            </div>
+
+            <?php
+            foreach ($itinerary as $index => $location) {
+                if ($index > 0) {
+                    $combinedname = $itinerary[$index]['M']['city']['S'] . ', ' . $itinerary[$index]['M']['country']['S'];
+                    $arrdate = (new DateTime($itinerary[$index - 1]['M']['date']['S']))->format('jS F Y');
+                    $depdate = (new DateTime($itinerary[$index]['M']['date']['S']))->format('jS F Y');
+                    echo '<div>
+                        <p><strong>Destination ' . $index . '</strong></p>
+                        <p>' . $combinedname . '</p>
+                        <p><strong>Arrive on </strong>' . $arrdate . '</p>
+                        <p><strong>Depart on </strong>' . $depdate . '</p>
+                    </div>
+                    <hr/>';
+                }
             }
-        }
-        ?>
+            ?>
 
-        <p><strong>Return Destination</strong></p>
-        <p><?= $itinerary[0]['M']['city']['S'] . ', ' . $itinerary[0]['M']['country']['S'] ?></p>
-        <p><strong>Arrive on </strong><?= (new DateTime($itinerary[count($itinerary) - 1]['M']['date']['S']))->format('jS F Y') ?></p>
-        <hr/>
+            <p><strong>Return Destination</strong></p>
+            <p><?= $itinerary[0]['M']['city']['S'] . ', ' . $itinerary[0]['M']['country']['S'] ?></p>
+            <p><strong>Arrive on </strong><?= (new DateTime($itinerary[count($itinerary) - 1]['M']['date']['S']))->format('jS F Y') ?></p>
+            <hr>
 
-        <form action='user.php'><input type='submit' value='Back'></form>
-        <form action='email.php'><input type='submit' value='Email to me'></form>
-    </body>
+            <form action="user.php" method="POST"><input class="form-control" type="submit" value="Back"></form>
+            <form action="email.php" method="POST"><input class="form-control" type="submit" value="Email to me"></form>
+        </div>
+        <div class="col-lg-3"></div>
+    </div>
+</body>
+
+<footer class="container-fluid" id="footer">
+    <div>
+        <p>COSC2626 Cloud Computing - Assessment 3</p>
+        <p><span>&#169;</span>James Dimos (s3722398)<br>
+            <span>&#169;</span>Louis Manabat (s3719633)
+        </p>
+    </div>
+</footer>
+
 </html>
